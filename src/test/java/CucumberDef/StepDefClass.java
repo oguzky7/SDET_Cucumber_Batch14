7afe7b5e4afbb4ebe99f773075baa52f903dcf37
oguzky7 <oguzky7@gmail.com>
2022-12-05 17:26:52 -0500

    First Commit check

--- src/test/java/CucumberDef/StepDefClass.java
+++ src/test/java/CucumberDef/StepDefClass.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 public class StepDefClass {
+    //Git trial
     WebDriver driver;
 
     @Given("user is navigated to HRMS application")
